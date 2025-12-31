5a88b09014e33dc4bad994b320d16c783f632110
Piyush Kedare <kedarepiyush82@gmail.com>
2025-12-31 12:23:20 +0530

    Added Level 2 Task 1 Calculator

---
+++ Level-2/Task-1/script.js
@@ -0,0 +1,19 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    alert("Invalid Expression");
+    clearDisplay();
+  }
+}
+
